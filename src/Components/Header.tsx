db6f95dfc68bcc4a8bd0902a349a3bc90ab28bfa
Freddy In Hsinchu Home <Freddy990117@gmail.com>
2026-02-02 07:43:11 +0800

    feat : add header layout & css

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="header-logo">
+        <img src="/public/icons/icon.png" alt="logo image" />
+        <h1>SUN CO.</h1>
+      </div>
+      <div className="header-cart">
+        <div className="header-cart-info">
+          <img src="/public/icons/shope-bag.png" alt="shop bag image" />
+          <button>View Cart</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
