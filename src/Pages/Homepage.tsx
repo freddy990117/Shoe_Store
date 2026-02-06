8f3563e217c22570aaf3ac26b56dd29d58eb342d
Freddy in Zhudong <freddy990117@gmail.com>
2026-02-06 09:30:39 +0800

    Feat : build product introduce layout

--- src/Pages/Homepage.tsx
+++ src/Pages/Homepage.tsx
@@ -4,7 +4,17 @@ const Homepage = () => {
   return (
     <div>
       <div className="homepage-main">
-        <div className="homepage-product"></div>
+        <div className="homepage-product">
+          <div className="product-introduce">
+            <h2>25% OFF</h2>
+            <h3>Summer Sale</h3>
+            <p>Discover our summer styles with discount</p>
+            <button>Shop Now</button>
+          </div>
+          <div className="product-image">
+            <img src="/public/images/picture-1.svg" alt="shoes picture" />
+          </div>
+        </div>
         <div className="homepage-more"></div>
       </div>
     </div>
