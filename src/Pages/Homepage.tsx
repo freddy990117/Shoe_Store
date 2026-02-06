941c4f671d9a8b1dcfbcc885962c35ab580388ac
Freddy in Zhudong <freddy990117@gmail.com>
2026-02-06 08:33:06 +0800

    Feat : Add Homepage layout

--- src/Pages/Homepage.tsx
+++ src/Pages/Homepage.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <div className="homepage-main">
+        <div className="homepage-product"></div>
+        <div className="homepage-more"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Homepage;
