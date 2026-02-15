7bc525f508ad7559ec34b1528f58f8bd287641ba
Freddy In Taichung <freddy990117@gmail.com>
2026-02-15 11:15:08 +0800

    Layout : update product layout. (v.1)

--- src/Pages/Product.tsx
+++ src/Pages/Product.tsx
@@ -1,7 +1,35 @@
 import React from "react";
 
 const Product = () => {
-  return <div>Product</div>;
+  return (
+    <div className="product-container">
+      <div className="product-gallery">
+        <div className="product-image">
+          <img src="" alt="product-intro" />
+          <button>Left</button>
+          <button>Right</button>
+          {/* You can put more picture to click over here. */}
+        </div>
+        <div className="prodduct-info">
+          <h3>shoe catalog</h3>
+          <h4>shoe des</h4>
+          <p>price</p>
+          <div className="product-lan">{/* border div */}</div>
+          <h5>Quanity</h5>
+          <div className="product-number">
+            <button>-</button>
+            <span>{/* click button would be changed */}</span>
+            <button>+</button>
+          </div>
+          <button>Add to Cart</button>
+        </div>
+      </div>
+      <div className="product-description">
+        <h1>Description</h1>
+        <div className="">{/* Border over here */}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
