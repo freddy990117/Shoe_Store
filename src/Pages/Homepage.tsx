022cb3ad76ddc0762393043c7e188b6b8e876cc3
Freddy In Taichung <freddy990117@gmail.com>
2026-02-15 10:02:04 +0800

    Feat : setup Link to product function.

--- src/Pages/Homepage.tsx
+++ src/Pages/Homepage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Homepage = () => {
   return (
     <div>
@@ -19,30 +19,38 @@ const Homepage = () => {
           <div className="more-product">
             <h2>Explore our latest drops</h2>
             <div className="product">
-              <div className="more-product">
-                <img src="/images/picture-2.png" alt="Nike Off-whi" />
-                <h3>Off-White</h3>
-                <h4>Out Of Office "Ooo" sneakers</h4>
-                <p>$775</p>
-              </div>
-              <div className="more-product">
-                <img src="/images/picture-3.png" alt="Nike shoes" />
-                <h3>Nike</h3>
-                <h4>Nike Air Force Premium</h4>
-                <p>$200</p>
-              </div>
-              <div className="more-product">
-                <img src="/images/picture-4.png" alt="Nike shoes" />
-                <h3>Nike</h3>
-                <h4>Nike Air Force Premium</h4>
-                <p>$98.23</p>
-              </div>
-              <div className="more-product">
-                <img src="/images/picture-5.png" alt="adidas shoes" />
-                <h3>adidas</h3>
-                <h4>DAILY 3.0 SHOES</h4>
-                <p>$98.99</p>
-              </div>
+              <Link to={`/product`}>
+                <div className="more-product">
+                  <img src="/images/picture-2.png" alt="Nike Off-whi" />
+                  <h3>Off-White</h3>
+                  <h4>Out Of Office "Ooo" sneakers</h4>
+                  <p>$775</p>
+                </div>
+              </Link>
+              <Link to={`/product`}>
+                <div className="more-product">
+                  <img src="/images/picture-3.png" alt="Nike shoes" />
+                  <h3>Nike</h3>
+                  <h4>Nike Air Force Premium</h4>
+                  <p>$200</p>
+                </div>
+              </Link>
+              <Link to={`/product`}>
+                <div className="more-product">
+                  <img src="/images/picture-4.png" alt="Nike shoes" />
+                  <h3>Nike</h3>
+                  <h4>Nike Air Force Premium</h4>
+                  <p>$98.23</p>
+                </div>
+              </Link>
+              <Link to={`/product`}>
+                <div className="more-product">
+                  <img src="/images/picture-5.png" alt="adidas shoes" />
+                  <h3>adidas</h3>
+                  <h4>DAILY 3.0 SHOES</h4>
+                  <p>$98.99</p>
+                </div>
+              </Link>
             </div>
           </div>
         </div>
