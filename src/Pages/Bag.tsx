32ccef60269acebdf50dde7faa39c427b4ce896a
Freddy in Zhudong <freddy990117@gmail.com>
2026-02-19 14:59:15 +0800

    Layout : update bag-summary layout

--- src/Pages/Bag.tsx
+++ src/Pages/Bag.tsx
@@ -56,8 +56,35 @@ const Bag = () => {
           <div className="shoe-lan" />
         </div>
       </div>
-
-      <div className="bag-summary"></div>
+      <div className="bag-summary">
+        <div className="bag-summary-info">
+          <h1>Summary</h1>
+          <div className="bag-summary-detail Subtotal">
+            <h3>Subtotal</h3>
+            <span>$90.00</span>
+          </div>
+          <div className="bag-summary-detail Shipping">
+            <h3>Shipping and delivery</h3>
+            <span>$20.00</span>
+          </div>
+          <div className="bag-summary-detail Tax">
+            <h3>Tax</h3>
+            <span>$6.00</span>
+          </div>
+          <div className="bag-summary-detail Discount">
+            <h3>Discount</h3>
+            <span>-$6.00</span>
+          </div>
+        </div>
+        <div className="bag-summary-lan"></div>
+        <div className="bag-summary-total">
+          <div className="total-detail">
+            <h2>Total</h2>
+            <span>$164.46</span>
+          </div>
+          <button className="checkout">Checkout →</button>
+        </div>
+      </div>
     </div>
   );
 };
