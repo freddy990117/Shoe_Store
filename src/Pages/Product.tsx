739e223aada81ff1b8f56b04df0fbedf08a29711
Freddy In Hsinchu Home <Freddy990117@gmail.com>
2026-03-09 20:03:40 +0800

    Feat : find user clock by initalIndex.

--- src/Pages/Product.tsx
+++ src/Pages/Product.tsx
@@ -1,22 +1,36 @@
 import { useProducts } from "../Context/ProductContext";
 import { useCallback, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const Product = () => {
+  // import props
   const productList = useProducts();
+  // setup Router's id (用於判斷目前點擊到那邊)
+  const { id } = useParams<{ id: string }>();
+  // Find id
+  const initalIndex = productList.findIndex((item) => item.id === Number(id));
+  // 判斷 id
+  const [currentIndex, setCurrectIndex] = useState<number>(
+    initalIndex >= 0 ? initalIndex : 0,
+  );
+
+  // find id by clicked
+  if (!productList) return <div>Product is delivering</div>;
 
   // right and left btn fn
-  const [currentIndex, setCurrectIndex] = useState<number>(0);
   const rightBtn = useCallback(() => {
     setCurrectIndex((prev) => {
       return prev < productList.length - 1 ? prev + 1 : prev;
     });
   }, []);
+
   const leftBtn = useCallback(() => {
     setCurrectIndex((prev) => {
       return prev > 0 ? prev - 1 : 0;
     });
   }, []);
-  // 	TS 會從 productList 自動推斷型別
+
+  // 	TS 會從 productList 自動推斷型別，並會自動取得 id
   const currentProduct = productList[currentIndex];
   const morePicture = productList.map((item) => {
     return <img src={`${item.imageURL}`} alt={`${item.title}`} key={item.id} />;
@@ -32,9 +46,11 @@ const Product = () => {
           <button className="right-btn" onClick={rightBtn}>
             {">"}
           </button>
-          <img src={`${currentProduct.imageURL}`} alt="product-intro" />
+          <img
+            src={`${currentProduct.imageURL}`}
+            alt={`${currentProduct.title}`}
+          />
           <div className="more-picture">{morePicture}</div>
-          {/* You can put more picture to click over here. */}
         </div>
         <div className="product-info">
           <div className="information">
@@ -65,11 +81,3 @@ const Product = () => {
 };
 
 export default Product;
-
-// import { useParams } from "react-router-dom";
-
-//  // 讀取 Router 中的 id 值
-//   const { id } = useParams<{ id: string }>();
-//   // find id by clicked
-//   const correctProduct = productList.find((item) => item.id === Number(id));
-//   if (!productList) return <div>Product is delivering</div>;
