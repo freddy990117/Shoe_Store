fee428a44efb962459fdd1ce25da1d59df83ad2f
Freddy990117 <Freddy990117@gmail.com>
2026-04-03 18:58:26 +0800

    Chore : add admin Provider on routers.

--- src/Router/Router.tsx
+++ src/Router/Router.tsx
@@ -6,22 +6,27 @@ import Bag from "../Pages/Bag";
 import { Layout } from "../Components/Layout";
 import { ProductProvider } from "../Context/ProductContext";
 import { CartProvider } from "../Context/CartContext";
+import { AdminProvider } from "../Context/AdminContext";
+import AdminPage from "../Pages/AdminPage";
 const Router = () => {
   return (
-    <CartProvider>
-      <ProductProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<Homepage />} />
-              <Route path="/product/:id" element={<Product />} />
-              <Route path="/bag" element={<Bag />} />
-              <Route path="*" element={<Page404 />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </ProductProvider>
-    </CartProvider>
+    <AdminProvider>
+      <CartProvider>
+        <ProductProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Layout />}>
+                <Route index element={<Homepage />} />
+                <Route path="/product/:id" element={<Product />} />
+                <Route path="/bag" element={<Bag />} />
+                <Route path="*" element={<Page404 />} />
+                <Route path="admin" element={<AdminPage />} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </ProductProvider>
+      </CartProvider>
+    </AdminProvider>
   );
 };
 
